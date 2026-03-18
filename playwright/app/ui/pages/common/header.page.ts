ecf8f01b96d02d413b0475874a294146720720f6
DimaSmaha <dmytrosmaha@gmail.com>
2026-03-18 14:24:38 +0200

    edit

--- playwright/app/ui/pages/common/header.page.ts
+++ playwright/app/ui/pages/common/header.page.ts
@@ -3,7 +3,6 @@ import Common from "./common.page";
 import { Wait } from "../../components/commonComponents/wait.component";
 import { Loader } from "../../components/commonComponents/loader.component";
 import { PageActions } from "../../components/commonComponents/pageActions.component";
-import { TableActionButtons } from "../../components/commonComponents/tableButtons.component";
 import { SelectOptions } from "../../components/commonComponents/selectOptions.component";
 import { LocatorsBuilders } from "../../components/commonComponents/locatorBuilders.component";
 
@@ -11,7 +10,6 @@ abstract class Header extends Common {
   public wait: Wait;
   public loader: Loader;
   public pageActions: PageActions;
-  public tableButtons: TableActionButtons;
   public selectOptions: SelectOptions;
   public locatorBuilders: LocatorsBuilders;
 
@@ -20,7 +18,6 @@ abstract class Header extends Common {
     this.wait = new Wait(page);
     this.loader = new Loader(page);
     this.pageActions = new PageActions(page);
-    this.tableButtons = new TableActionButtons(page);
     this.selectOptions = new SelectOptions(page);
     this.locatorBuilders = new LocatorsBuilders(page);
   }
